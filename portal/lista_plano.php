9b8447a8fde535c04eb2d48a46ae7122d0e5181a
amanda-bampi8 <amanda-dbampi@estudante.rs.gov.br>
2025-10-07 09:49:02 -0300

    push

--- portal/lista_plano.php
+++ portal/lista_plano.php
@@ -1,86 +1,92 @@
 <?php
-    include('verifica_login.php');
-    include('conexao.php');
+        include('verifica_login.php');
+        include('conexao.php');
 
-    $niveldapagina = array($mestre, $colaborador);
+        $niveldapagina = array($mestre, $colaborador);
 
-    if(!in_array ($nivel, $niveldapagina)) {
-        echo
-            "<script> 
-            alert('Você não tem acesso a essa área!')
-            history.go(-1);
-            </script>";
-    }
-?>
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head> 
-        <title> INÍCIO | Meu Portal </title>
-        <!------------------------------------ HEAD ------------------------------------>
-        <?php include 'include-portal/head.php'; ?>
-        <!------------------------------------ HEAD ------------------------------------>
-    </head>
-    <body>
-        <header>
-            <!--------------------------------- HEADER --------------------------------->
-            <?php include 'include-portal/header.php'; ?>
-            <!--------------------------------- HEADER --------------------------------->
-        </header>
-        
-        <div class="container">
-            <br>
+        if(!in_array ($nivel, $niveldapagina)) {
+            echo
+                "<script> 
+                alert('Você não tem acesso a essa área!')
+                history.go(-1);
+                </script>";
+        }
+    ?>
+    <!DOCTYPE html>
+    <html lang="pt-br">
+        <head> 
+            <title> INÍCIO | Meu Portal </title>
+            <!------------------------------------ HEAD ------------------------------------>
+            <?php include 'include-portal/head.php'; ?>
+            <!------------------------------------ HEAD ------------------------------------>
+        </head>
+        <body>
+            <header>
+                <!--------------------------------- HEADER --------------------------------->
+                <?php include 'include-portal/header.php'; ?>
+                <!--------------------------------- HEADER --------------------------------->
+            </header>
+            
+            <div class="container">
+                <br>
+
                 <?php
-                    $consulta = "SELECT * FROM planos
-                                    ORDER BY nome_plano ASC";
-                    $con = mysqli_query($conexao, $consulta) OR DIE ($conexao->error);
+                    $consulta = "SELECT * FROM planos 
+                                ORDER BY nome_plano ASC";
+
+                    $con   = mysqli_query($conexao,  $consulta) OR DIE ($conexao->error);
+
                     if(($con) AND ($con->num_rows != 0)) {
                 ?>
-            <center><h5> LISTA DE PLANOS CADASTRADOS</h5></center>
 
-            <hr>
+                <center><h5> LISTA DE PLANOS CADASTRADOS </h5></center>
+
+                <hr>
+
                 <?php
-                if (isset($_SESSION['msgListaPlano'])) {
-                    echo  $_SESSION['msgListaPlano'];
+                if (isset ($_SESSION['msgListaPlano'])) {
+                    echo $_SESSION['msgListaPlano'];
                     unset($_SESSION['msgListaPlano']);
+
                 }
                 ?>
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Plano</th>
-                        <th scope="col">Descrição</th>
-                        <th scope="col">Valor</th>
-                        <th scope="col">editar</th>
-                        <th scope="col">excluir</th>
 
+                    <table class="table table-hover">
+        <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">Plano</th>
+            <th scope="col">Descrição</th>
+            <th scope="col">Valor</th>
+            <th scope="col">Editar</th>
+            <th scope="col">Excluir</th>
+            </tr>
+        </thead>
+        <?php  while($dado = mysqli_fetch_assoc($con)) {?>
+        <tbody>
+            <tr>
+            <th scope="row"><?php echo $dado['id_plano']; ?></th>
+            <td><?php echo $dado['nome_plano']; ?></td>
+            <td><?php echo $dado['desc_plano']; ?></td>
+            <td><?php echo $dado['vlr_plano'];  ?></td>
+            <td> <a href= "edt_plano.php?id_plano=<?php echo $dado['id_plano']; ?>"><i class='fa fa-edit' style='font-size:24px'></i></a></td>
+            <td><i class='fa fa-trash' style='font-size:24px'></td>
+            </tr>
+        </tbody>
+        <?php } ?>
+        </table>
 
-                    </tr>
-                </thead>
-                <tbody>
-                <?php while($dado = mysqli_fetch_assoc($con)) { ?>
-                    <tr>
-                    <th scope="row"><?php echo $dado['id_plano']; ?></th>
-                    <td><?php echo $dado['nome_plano']; ?></td>
-                    <td><?php echo $dado['desc_plano']; ?></td>
-                    <td><?php echo $dado['vlr_plano'];  ?></td>
-                    <td> <i class='fa fa-edit'style='font-size:24px'></i></td>
-                    <td> <i class='fa fa-trash'style='font-size:24px'></i></td>
-
-                    </tr>
-                </tbody>
-                <?php } ?>
-                </table>
-                <?php } else {
-                    echo "<p> NENHUM PPLANO CADASTRADO </p>";
-                }?>
-            <br>
-            
-        </div>
-        <footer>
-            <!------------------------------- FOOTER ----------------------------------->
-            <?php include 'include-portal/footer.php'; ?>
-            <!------------------------------- FOOTER ----------------------------------->
-        </footer>
-    </body>
-</html>
+        <?php } else {
+            echo "<p> nenhum plano cadastrado! </p>";
+        }
+        ?>
+                <br>
+                
+            </div>
+            <footer>
+                <!------------------------------- FOOTER ----------------------------------->
+                <?php include 'include-portal/footer.php'; ?>
+                <!------------------------------- FOOTER ----------------------------------->
+            </footer>
+        </body>
+    </html>
--- portal/save_plano.php
+++ portal/save_plano.php
@@ -5,25 +5,29 @@
     $niveldapagina= array($mestre, $colaborador);
 
     $niveldapagina = array($mestre, $colaborador);
+$id_plano      = filter_input(INPUT_GET, 'id_plano', FILTER_SANITIZE_NUMBER_INT);
 
     if(!in_array ($nivel, $niveldapagina)) {
         echo "<script> 
             alert('Você não tem acesso a essa área!')
             history.go(-1);
             </script>";
-    } else if (!empty($_POST['nome_plano']) OR !empty($_POST['desc_plano']) OR !empty($_POST['vlr_plano'])) {
-
+    } else if (!empty($id_plano)) {
+ 
     $nome_plano = $_POST['nome_plano'];
     $desc_plano = $_POST['desc_plano'];
     $vlr_plano = $_POST['vlr_plano'];
 
-    $sql_code = "INSERT INTO planos (nome_plano, desc_plano, vlr_plano, dt_cad_plano) VALUES ('$nome_plano' , '$desc_plano' , '$vlr_plano' , NOW())";
+    $consultaplano = "SELECT * FROM planos
+        WHERE id_plano = '$id_plano'";
+
+    $sql_code = "UPDATE planos SET nome_plano='$nome_plano', desc_plano='$desc_plano', vlr_plano='$vlr_plano' WHERE id_plano = '$id_plano'";
     if ($conexao->query($sql_code)or die($conexao->error)) {
-        $_SESSION['msgFormPlano'] = "<center> <div class='alert alert-success' role='alert'> OK, plano inserido com sucesso </div> </center>";
+        $_SESSION['msgListaPlano'] = "<center> <div class='alert alert-success' role='alert'> OK, plano inserido com sucesso </div> </center>";
         header("Location: lista_plano.php");
     } else {
-            $_SESSION['msgFormPlano'] = "<center> <div class='alert alert-danger' role='alert'>ERRO, plano inserido com sucesso </div> </center>";
-            header("Location: form_plano.php");
+            $_SESSION['msgListaPlano'] = "<center> <div class='alert alert-danger' role='alert'>ERRO, plano inserido com sucesso </div> </center>";
+            header("Location: lista_plano.php");
 
     }
 
